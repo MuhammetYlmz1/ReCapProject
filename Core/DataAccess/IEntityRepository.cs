91c88d95d60ad3cf7216c7c5f7113dd61b9b6e35
MuhammetYlmz1 <muhammetylmz278@gmail.com>
2021-03-18 15:33:53 +0300

    Back-End

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace Core.DataAccess
 {
-   public interface IEntityRepository<T> where T:class,IEntity,new()
+   public interface IEntityRepository<T> where T:class,IEntity,new() 
     {
         List<T> GetAll(Expression<Func<T,bool>>filter=null);
         void Add(T entity);
