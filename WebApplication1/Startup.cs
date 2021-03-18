98deb3b4ab8f81c269e796b35d8b9ae5e3ae2472
MuhammetYlmz1 <muhammetylmz278@gmail.com>
2021-03-18 15:31:19 +0300

    Back-End

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -78,7 +78,7 @@ namespace WebApplication1
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) 
         {
             if (env.IsDevelopment())
             {
